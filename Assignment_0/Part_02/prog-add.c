b6a6e582ede08f6cc3cf90b98a5792f7965b5a39
Bhavya <bhavya18333@iiitd.ac.in>
2019-12-29 15:00:25 +0530

    initial prog-add.c script

---
+++ Assignment_0/Part_02/prog-add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	int a, b;
+	printf("Enter integer A: ");
+	scanf("%d", &a);
+	printf("Enter integer B: ");
+	scanf("%d", &b);
+	printf("%d, %d\n", a, b);
+	return(0);
+}
